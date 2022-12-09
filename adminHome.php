86196dae0c5d4265286182bf1305cb60b3820dfe
stevenaitken <stevenaitken@hotmail.com>
2022-12-09 11:44:56 +0000

    Add files via upload

---
+++ adminHome.php
@@ -0,0 +1,50 @@
+<?php session_start(); include('includes/error-reporting.php');include('includes/connx.php');
+
+
+$date = date('d-m-y H:i:s');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php  include('modules/head.php'); ?>
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+
+<body>
+    <?php
+   
+
+?>
+    <div class="container">
+        <h2>Admin Home</h2>
+        
+        <p> You are logged in as: <?php echo $_SESSION['username']. " on ". $date; ?> </p>
+
+     <div class = "cpanel">
+
+<div class="optionBox">1</div>
+<div class="optionBox">2</div>
+<div class="optionBox">3</div>
+<div class="optionBox">4</div>
+<div class="optionBox">5</div>
+<div class="optionBox">6</div>
+
+
+
+
+
+     </div>
+       
+
+
+    </div>
+
+</body>
+
+</html>
+
