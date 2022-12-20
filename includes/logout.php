d35de112490cd3540327a759a6f74212bbad7333
stevenaitken <stevenaitken@hotmail.com>
2022-12-20 08:54:43 +0000

    Delete includes directory

--- includes/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // start the session
-session_unset(); // unset the session
-session_destroy(); // destroy the session
-header('location:login-verify-error-session.php');
-?>
